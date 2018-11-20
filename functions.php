a95a07a2a255619df262b2dd1002c2d633757f6a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-11-20 17:25:44 -0200

    Criação de funções básicas

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+// Registra os menus
+register_nav_menus(
+    array(
+        'main' => 'Menu Principal',
+    )
+);
+
+// Post Thumbnail
+require_once('inc/post-thumbnails.php');
+
+// Breadcrumb
+require_once('inc/breadcrumb.php');
+
+// Script Condicional
+require_once('inc/script_conditional.php');
+
+// Scripts & Styles
+require_once('inc/assets.php');
---
+++ inc/assets.php
@@ -0,0 +1,27 @@
+<?php
+function memoria_load_styles() {
+    /* wp_enqueue_style( $handle, $src, $deps, $ver, $media ); */
+
+    wp_enqueue_style('css-memoria', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/memoria.css' : '/css/memoria.min.css'), array(), false, 'all');
+}
+
+function memoria_load_scripts() {
+    /* wp_register_script( $handle, $src, $deps, $ver, $in_footer ); */
+    /* wp_enqueue_script( $handle[, $src, $deps, $ver, $in_footer] ); */
+
+    if (!is_admin()) {
+        wp_deregister_script('jquery');
+    }
+
+    wp_enqueue_script( 'js-ie', get_template_directory_uri().(WP_DEBUG ? '/js/ie.js' : '/js/ie.min.js'), array(), null, false );
+    wp_script_add_data( 'js-ie', 'conditional', 'lt IE 9' );
+
+    wp_enqueue_script('js-memoria', get_template_directory_uri().(WP_DEBUG ? '/js/memoria.js' : '/js/memoria.min.js'), array(), null, true);
+
+    if (!WP_DEBUG) {
+        wp_enqueue_script( 'js-barra-brasil', '//barra.brasil.gov.br/barra.js', array(), null, true );
+    }
+}
+
+add_action( 'wp_enqueue_scripts', 'memoria_load_styles', 1 );
+add_action( 'wp_enqueue_scripts', 'memoria_load_scripts', 1 );
