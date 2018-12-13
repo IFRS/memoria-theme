d1cc675d8b67e95af27f1c007ee142f77b8a0a8b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-12-13 14:12:58 -0200

    Controle da exibição da coluna para voltar ao topo na linha do tempo

--- footer.php
+++ footer.php
@@ -2,17 +2,20 @@
 
                 <a href="#fim-conteudo" id="fim-conteudo" class="sr-only">Fim do conte&uacute;do</a>
             </div> <!-- /.col -->
-            <div class="col-12 col-md-1 col-lg-2">
-                <div class="h-100 d-flex flex-wrap">
-                    <hr class="align-self-start d-none d-md-block back-top__bar">
-                    <a href="#inicio-conteudo" class="align-self-end back-top__link">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
-                            <path d="M4 0l-4 4 1.5 1.5 2.5-2.5 2.5 2.5 1.5-1.5-4-4z" transform="translate(0 1)" />
-                        </svg>
-                        <span class="sr-only">Voltar para o topo</span>
-                    </a>
+
+            <?php if (!is_post_type_archive( 'registro' )) : ?>
+                <div class="col-12 col-md-1 col-lg-2">
+                    <div class="h-100 d-flex flex-wrap">
+                        <hr class="align-self-start d-none d-md-block back-top__bar">
+                        <a href="#inicio-conteudo" class="align-self-end back-top__link">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
+                                <path d="M4 0l-4 4 1.5 1.5 2.5-2.5 2.5 2.5 1.5-1.5-4-4z" transform="translate(0 1)" />
+                            </svg>
+                            <span class="sr-only">Voltar para o topo</span>
+                        </a>
+                    </div>
                 </div>
-            </div>
+            <?php endif; ?>
         </div> <!-- /.row -->
     </div> <!-- /.container-fluid -->
 
