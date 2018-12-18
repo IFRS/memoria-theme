6053521f0cf847d7cd78583492dd7ed9b23e3f69
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-12-18 09:06:50 -0200

    Adição de mais imagens de fundo

--- src/modules/bg.js
+++ src/modules/bg.js
@@ -1,5 +1,5 @@
 $(function() {
-    var classes = ['bg1', 'bg2'];
+    var classes = ['bg1', 'bg2', 'bg3', 'bg4'];
     var bg = classes[Math.floor(Math.random() * classes.length)];
     $('.main-wrapper').addClass(bg);
 });
