6cdfd16664aa5e7fcd1d5e596045abd7688c9364
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-12-19 15:34:09 -0200

    Criação do template para erro 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<main class="content">
+    <h2 class="sr-only">P&aacute;gina n&atilde;o encontrada</h2>
+    <div class="alert alert-danger" role="alert">
+        <p><strong>Ops!&nbsp;</strong>A p&aacute;gina que voc&ecirc; procura n&atilde;o foi encontrada. Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.</p>
+    </div>
+</main>
+
+<?php get_footer(); ?>
