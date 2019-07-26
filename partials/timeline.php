f736fd77f7302c6c888863be6b8245c55ee2bd2f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-07-26 17:42:15 -0300

    Repaginação da Timeline usando tabs do Bootstrap

--- partials/timeline.php
+++ partials/timeline.php
@@ -12,68 +12,70 @@
 
 <main class="timeline">
     <h2 class="sr-only">Linha do Tempo</h2>
-    <ul class="swiper-pagination"></ul>
-    <div class="swiper-container">
-        <div class="swiper-wrapper">
-            <?php foreach ($posts_by_year as $year => $posts) : ?>
-                <div class="swiper-slide" data-year="<?php echo $year; ?>">
-                    <div class="swiper-content">
-                        <?php foreach ($posts as $post) : ?>
-                            <div class="swiper-slide-content">
-                                <span class="timeline-date"><?php echo get_the_date('d \d\e F', $post->ID); ?></span>
-                                <?php
-                                    if (has_post_thumbnail($post->ID)) {
-                                        echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'img-fluid timeline-image'));
-                                    }
-                                ?>
-                                <h3 class="timeline-title"><a href="<?php echo get_the_permalink($post->ID); ?>" data-toggle="modal" data-target="#modal-<?php echo $post->ID; ?>"><?php echo $post->post_title; ?></a></h3>
-                                <div class="timeline-text"><?php echo get_the_excerpt($post->ID); ?></div>
-                                <?php $unidades = get_the_terms($post->ID, 'unidade'); ?>
-                                <?php if (!empty($unidades)) : ?>
-                                    <?php foreach ($unidades as $unidade) : ?>
-                                        <a class="timeline-link" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
-                                    <?php endforeach; ?>
-                                <?php endif; ?>
-                            </div>
-                            <?php add_action('wp_footer', function() use ($post, $unidades) { ?>
-                                <div class="modal fade" id="modal-<?php echo $post->ID; ?>" tabindex="-1" role="dialog" aria-labelledby="modal-<?php echo $post->ID; ?>-title" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h4 class="modal-title" id="modal-<?php echo $post->ID; ?>-title"><?php echo $post->post_title; ?></h4>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="row">
-                                                    <div class="col-12 col-md-6">
-                                                        <p>
-                                                        <?php foreach ($unidades as $unidade) : ?>
-                                                            <a class="timeline-link align-self-start" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
-                                                        <?php endforeach; ?>
-                                                        </p>
-                                                    </div>
-                                                    <div class="col-12 col-md-6"><p class="text-right"><strong><?php echo get_the_date('', $post->ID); ?></strong></p></div>
+    <ul class="timeline__ano-list nav" role="tablist">
+        <?php foreach ($posts_by_year as $year => $posts) : ?>
+            <li class="timeline__ano-item nav-item"><a class="timeline__ano-link nav-link<?php echo (reset($posts_by_year) === $posts) ? ' active' : ''; ?>" href="#collapse-<?php echo $year; ?>" role="tab" aria-selected="false" data-toggle="tab"><?php echo $year; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+    <div class="tab-content">
+        <?php foreach ($posts_by_year as $year => $posts) : ?>
+            <div class="tab-pane fade<?php echo (reset($posts_by_year) === $posts) ? ' active show' : ''; ?>" id="collapse-<?php echo $year; ?>" role="tabpanel">
+                <div class="timeline__ano-content">
+                    <?php foreach ($posts as $post) : ?>
+                        <div class="registro">
+                            <span class="registro__date"><?php echo get_the_date('d \d\e F', $post->ID); ?></span>
+                            <?php
+                                if (has_post_thumbnail($post->ID)) {
+                                    echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'img-fluid registro__image'));
+                                }
+                            ?>
+                            <h3 class="registro__title"><a href="<?php echo get_the_permalink($post->ID); ?>" data-toggle="modal" data-target="#modal-<?php echo $post->ID; ?>"><?php echo $post->post_title; ?></a></h3>
+                            <div class="registro__text"><?php echo get_the_excerpt($post->ID); ?></div>
+                            <?php $unidades = get_the_terms($post->ID, 'unidade'); ?>
+                            <?php if (!empty($unidades)) : ?>
+                                <?php foreach ($unidades as $unidade) : ?>
+                                    <a class="registro__link" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </div>
+                        <?php add_action('wp_footer', function() use ($post, $unidades) { ?>
+                            <div class="modal fade" id="modal-<?php echo $post->ID; ?>" tabindex="-1" role="dialog" aria-labelledby="modal-<?php echo $post->ID; ?>-title" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="modal-<?php echo $post->ID; ?>-title"><?php echo $post->post_title; ?></h4>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="row">
+                                                <div class="col-12 col-md-6">
+                                                    <p>
+                                                    <?php foreach ($unidades as $unidade) : ?>
+                                                        <a class="registro__link align-self-start" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
+                                                    <?php endforeach; ?>
+                                                    </p>
                                                 </div>
-                                                <?php
-                                                    if (has_post_thumbnail($post->ID)) {
-                                                        echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'mb-3 img-fluid timeline-image'));
-                                                    }
-                                                ?>
-                                                <?php echo $post->post_content; ?>
+                                                <div class="col-12 col-md-6"><p class="text-right"><strong><?php echo get_the_date('', $post->ID); ?></strong></p></div>
                                             </div>
-                                            <div class="modal-footer">
-                                                <small><strong>Atualizado em: </strong><?php echo get_the_modified_date('', $post->ID); ?> &agrave;s <?php echo get_the_modified_time('', $post->ID); ?></small>
-                                            </div>
+                                            <?php
+                                                if (has_post_thumbnail($post->ID)) {
+                                                    echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'mb-3 img-fluid registro__image'));
+                                                }
+                                            ?>
+                                            <?php echo $post->post_content; ?>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <small><strong>Atualizado em: </strong><?php echo get_the_modified_date('', $post->ID); ?> &agrave;s <?php echo get_the_modified_time('', $post->ID); ?></small>
                                         </div>
                                     </div>
                                 </div>
-                            <?php }); ?>
-                        <?php endforeach; ?>
-                    </div>
+                            </div>
+                        <?php }); ?>
+                    <?php endforeach; ?>
                 </div>
-            <?php endforeach; ?>
-        </div>
+            </div>
+        <?php endforeach; ?>
     </div>
 </main>
