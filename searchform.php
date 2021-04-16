6687c71cb056519c20acb280b10e0c8891fc8114
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-16 17:12:54 -0300

    Ajuste no ícone de busca

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="<?php echo $field_id; ?>" class="form-control form-control-sm border-light searchform__field" placeholder="Busque uma memória..." required>
         <span class="input-group-append">
             <button type="submit" class="btn btn-light btn-sm searchform__submit" title="Buscar" value="Buscar">
-                <svg xmlns="http://www.w3.org/2000/svg" class="searchform__icon" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="searchform__icon" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <circle cx="10" cy="10" r="7" />
                     <line x1="21" y1="21" x2="15" y2="15" />
                 </svg>
