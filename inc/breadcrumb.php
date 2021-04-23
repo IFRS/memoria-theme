d97f22894a1920253a51cd4fdadda6034e21f5b1
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-23 12:54:29 -0300

    Reorganização do código para breadcrumb

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -1,25 +1,25 @@
 <?php
 function memoria_breadcrumb() {
-    $before    = '<li class="breadcrumb-item active" aria-current="page">';
-    $sep       = '';
-    $after     = '</li>';
+    $before_item   = '<li class="breadcrumb-item">';
+    $before_active = '<li class="breadcrumb-item active" aria-current="page">';
+    $after         = '</li>';
 
     if (!is_front_page() || is_paged()) {
-        echo '<nav class="breadcrumb-nav" aria-label="Breadcrumb">';
+        echo '<nav class="breadcrumb-nav" aria-label="Você está em:">';
 		echo '<ol class="breadcrumb container">';
-		//echo 'Voc&ecirc; est&aacute; em:&nbsp;';
 
         global $post;
         $homeLink = home_url();
 		$siteprincipal = get_home_url('1','/');
         $nomesite = get_bloginfo('name');
 
-        echo '<li class="breadcrumb-item">';
-        echo '<a href="' . $homeLink . '">' . $nomesite . '</a> ' . $sep;
-        echo '</li>';
+        echo $before_item;
+        echo '<svg class="breadcrumb-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9"/><path d="M9 22V12h6v10M2 10.6L12 2l10 8.6"/></svg>';
+        printf('<a href="%s">%s</a>', $homeLink, $nomesite);
+        echo $after;
 
         if (is_home()) {
-            echo $before . get_the_title(get_option( 'page_for_posts' )) . $after;
+            echo $before_active . get_the_title(get_option( 'page_for_posts' )) . $after;
         } elseif (is_category()) {
             global $wp_query;
             $cat_obj   = $wp_query->get_queried_object();
@@ -27,80 +27,70 @@ function memoria_breadcrumb() {
             $thisCat   = get_category($thisCat);
             $parentCat = get_category($thisCat->parent);
             if ($thisCat->parent != 0) {
-                echo get_category_parents($parentCat, true, $sep);
+                echo get_category_parents($parentCat, true, '');
             }
-            echo $before . single_cat_title('', false) . $after;
+            echo $before_active . single_cat_title('', false) . $after;
         } elseif (is_day()) {
-            echo '<li class="breadcrumb-item"><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time(
-                'Y'
-            ) . '</a></li> ';
-            echo '<li class="breadcrumb-item"><a href="' . get_month_link(get_the_time('Y'), get_the_time('m')) . '">' . get_the_time(
-                'F'
-            ) . '</a></li> ';
-            echo $before . get_the_time('d') . $after;
+            echo $before_item . '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a>' . $after;
+            echo $before_item . '<a href="' . get_month_link(get_the_time('Y'), get_the_time('m')) . '">' . get_the_time('F') . '</a>' . $after;
+            echo $before_active . get_the_time('d') . $after;
         } elseif (is_month()) {
-            echo '<li class="breadcrumb-item"><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time(
-                'Y'
-            ) . '</a></li> ';
-            echo $before . get_the_time('F') . $after;
+            echo $before_item . '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a>' . $after;
+            echo $before_active . get_the_time('F') . $after;
         } elseif (is_year()) {
-            echo $before . get_the_time('Y') . $after;
+            echo $before_active . get_the_time('Y') . $after;
         } elseif (is_single() && !is_attachment()) {
             if (get_post_type() != 'post') {
                 $post_type = get_post_type_object(get_post_type());
                 $slug      = $post_type->rewrite;
-                echo '<li class="breadcrumb-item"><a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->name . '</a></li> ';
-                echo $before . get_the_title() . $after;
+                echo $before_item . '<a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->name . '</a>' . $after;
+                echo $before_active . get_the_title() . $after;
             } else {
                 $cat = get_the_category();
                 $cat = $cat[0];
-                echo '<li class="breadcrumb-item">'.get_category_parents($cat, true, $sep).'</li>';
-                echo $before . get_the_title() . $after;
+                echo $before_item . get_category_parents($cat, true, '') . $after;
+                echo $before_active . get_the_title() . $after;
             }
         } elseif (!is_single() && !is_page() && get_post_type() != 'post' && !is_404()) {
             if (is_tax('unidade')) {
-                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'registro' ) . '">' . __('Linha do Tempo', 'ifrs-memoria-theme') . '</a></li>';
-                echo $before . single_term_title('', false) . $after;
+                echo $before_item . '<a href="' . get_post_type_archive_link( 'registro' ) . '">' . __('Linha do Tempo', 'ifrs-memoria-theme') . '</a>' . $after;
+                echo $before_active . single_term_title('', false) . $after;
             } else {
-                echo $before . post_type_archive_title('', false) . $after;
+                echo $before_active . post_type_archive_title('', false) . $after;
             }
         } elseif (is_attachment()) {
             $parent = get_post($post->post_parent);
             $cat    = get_the_category($parent->ID);
             $cat    = $cat[0];
-            echo get_category_parents($cat, true, $sep);
-            echo '<li class="breadcrumb-item"><a href="' . get_permalink(
-                $parent
-            ) . '">' . $parent->post_title . '</a></li> ';
-            echo $before . get_the_title() . $after;
+            echo get_category_parents($cat, true, '');
+            echo $before_item . '<a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a>' . $after;
+            echo $before_active . get_the_title() . $after;
 
         } elseif (is_page() && !$post->post_parent) {
-            echo $before . get_the_title() . $after;
+            echo $before_active . get_the_title() . $after;
         } elseif (is_page() && $post->post_parent) {
             $parent_id   = $post->post_parent;
             $breadcrumbs = array();
             while ($parent_id) {
                 $page          = get_page($parent_id);
-                $breadcrumbs[] = '<li class="breadcrumb-item"><a href="' . get_permalink($page->ID) . '">' . get_the_title(
-                    $page->ID
-                ) . '</a>' . $sep . '</li>';
+                $breadcrumbs[] = $before_item . '<a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>' . $after;
                 $parent_id     = $page->post_parent;
             }
             $breadcrumbs = array_reverse($breadcrumbs);
             foreach ($breadcrumbs as $crumb) {
                 echo $crumb;
             }
-            echo $before . get_the_title() . $after;
+            echo $before_active . get_the_title() . $after;
         } elseif (is_search()) {
-            echo $before . 'Resultado da pesquisa: "' . get_search_query() . '"' . $after;
+            echo $before_active . 'Resultado da pesquisa por "' . get_search_query() . '"' . $after;
         } elseif (is_tag()) {
-            echo $before . 'Posts tagged "' . single_tag_title('', false) . '"' . $after;
+            echo $before_active . 'Posts tagged "' . single_tag_title('', false) . '"' . $after;
         } elseif (is_author()) {
             global $author;
             $userdata = get_userdata($author);
-            echo $before . ' ' . $userdata->display_name . $after;
+            echo $before_active . ' ' . $userdata->display_name . $after;
         } elseif (is_404()) {
-            echo $before . 'Erro 404' . $after;
+            echo $before_active . 'Erro 404' . $after;
         }
 
         echo '</ol>';
