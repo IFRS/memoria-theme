bef90d943b77cb7c8914a85c2b91f57030a78c35
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-28 11:04:28 -0300

    Adição e estilização de paginação no conteúdo

--- page.php
+++ page.php
@@ -12,6 +12,15 @@
         ?>
         <?php the_content(); ?>
     </div>
+    <nav aria-label="Paginação do Conteúdo" class="text-center">
+        <?php
+            wp_link_pages(array(
+                'before' => '<ul class="pagination"><li class="page-item">',
+                'separator' => '</li><li class="page-item">',
+                'after'  => '</li></ul>',
+            ));
+        ?>
+    </nav>
 </article>
 
 <?php get_footer(); ?>
