0b249cb468230069d7eda9539cb1f0af8b918e7c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-29 13:59:28 -0300

    Ajuste na legenda das imagens que usam fancybox

--- src/modules/fancybox.js
+++ src/modules/fancybox.js
@@ -1,9 +1,13 @@
 require('@fancyapps/fancybox');
 
 $(function() {
-    $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif']").attr('data-fancybox', 'gallery').fancybox();
-    $("a[rel='gallery']").each(function() {
-        var caption = $(this).parent().next('.gallery-caption').text();
+    $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif']").attr('data-fancybox', 'gallery').each(function() {
+        let caption = $(this).next('.blocks-gallery-item__caption').text();
+
+        if (!caption) {
+            caption = $(this).parent().next('.wp-caption-text').text();
+        }
+
         if (caption) {
             $(this).attr('data-caption', $.trim(caption));
         }
