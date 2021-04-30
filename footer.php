c93cc2bbd2e4ec0523aea4f1372c2d8be766010f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-30 14:33:53 -0300

    Adição de title para links dos créditos

--- footer.php
+++ footer.php
@@ -22,19 +22,19 @@
             <div class="row">
                 <div class="col-12 creditos">
                     <!-- Wordpress -->
-                    <a href="http://br.wordpress.org/" target="_blank" rel="noopener">
+                    <a href="http://br.wordpress.org/" target="_blank" rel="noopener" title="Desenvolvido com Wordpress">
                         <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-wordpress.png" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)"/>
                         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
                     </a>
                     &mdash;
                     <!-- Git -->
-                    <a href="https://github.com/IFRS/memoria-theme/" target="_blank" rel="noopener">
+                    <a href="https://github.com/IFRS/memoria-theme/" target="_blank" rel="noopener" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
                         <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-git.png" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)"/>
                         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
                     </a>
                     &mdash;
                     <!-- Creative Commons -->
-                    <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noopener">
+                    <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noopener" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
                         <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-cc-by-nc-sa.png" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)"/>
                         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
                     </a>
