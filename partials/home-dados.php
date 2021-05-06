d39003ad5ce871582cd48333bb7b2b91830021cc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-06 13:34:58 -0300

    Melhorias na contagem de dados para a página inicial

--- partials/home-dados.php
+++ partials/home-dados.php
@@ -3,11 +3,12 @@
         <h2 class="home-dados__titulo"><?php bloginfo('name'); ?> em n&uacute;meros</h2>
         <div class="home-dados__content">
             <article class="dado">
-                <p class="dado__numero"><?php echo wp_count_posts( 'registro' )->publish; ?></p>
-                <p class="dado__texto">Datas registradas na Linha do Tempo</p>
+                <?php $registros = wp_count_posts( 'registro' )->publish ?? 0; ?>
+                <p class="dado__numero"><?php echo $registros; ?></p>
+                <p class="dado__texto"><?php echo _n('Data registrada', 'Datas registradas', $registros, 'ifrs-memoria-theme'); ?> na Linha do Tempo</p>
             </article>
             <article class="dado">
-                <?php $colecoes = wp_count_posts( 'tainacan-collection' )->publish; ?>
+                <?php $colecoes = wp_count_posts( 'tainacan-collection' )->publish ?? 0; ?>
                 <p class="dado__numero"><?php echo $colecoes; ?></p>
                 <p class="dado__texto"><?php echo _n('Coleção', 'Coleções', $colecoes, 'ifrs-memoria-theme'); ?> no Acervo</p>
             </article>
@@ -24,7 +25,7 @@
                 while ($collections->have_posts()) {
                     $collections->the_post();
                     $posts = wp_count_posts( 'tnc_col_' . get_the_ID() . '_item' );
-                    $total += ($posts) ? $posts->publish : 0;
+                    $total += $posts->publish ?? 0;
                 }
 
                 wp_reset_postdata();
