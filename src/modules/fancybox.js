24bfc83747ef4fc2b9bd78948d808b7b1988a570
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-13 13:14:22 -0300

    Diferenciação para galerias do WP no fancybox

--- src/modules/fancybox.js
+++ src/modules/fancybox.js
@@ -2,17 +2,25 @@ require('@fancyapps/fancybox');
 
 $(function() {
     $.fancybox.defaults.buttons = [
-        //"zoom",
+        "zoom",
         //"share",
         "slideShow",
         //"fullScreen",
         "download",
-        "thumbs",
+        //"thumbs",
         "close"
     ];
     $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']").each(function() {
         if ($(this).children('img').length === 1) {
             $(this).attr('data-fancybox', 'gallery');
+
+            wp_gallery = $(this).parents('.wp-block-gallery').first();
+            if (wp_gallery.length === 1) {
+                if (!wp_gallery.attr('id')) {
+                    wp_gallery.attr('id', 'gallery-' + Math.floor(Math.random() * 100));
+                }
+                $(this).attr('data-fancybox', wp_gallery.attr('id'));
+            }
 
             let caption = $(this).siblings('figcaption').first().text();
 
