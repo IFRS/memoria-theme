662912b1b8f0b052694290b1f2029d0609971366
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-13 16:51:50 -0300

    Ajuste no código

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,6 +1,6 @@
 $(function() {
-    $('.navbar-menu').each(function(i, menu) {
-        $(menu).find('.has-dropdown').each(function(k, dropdown) {
+    $('.navbar-menu').each(function(_i, menu) {
+        $(menu).find('.has-dropdown').each(function(_k, dropdown) {
             if ($(dropdown).children('.navbar-dropdown').length > 0) {
                 $(dropdown)
                     .children('a')
@@ -9,7 +9,7 @@ $(function() {
                         $(menu).find('.has-dropdown').not($(this).parent()).removeClass('is-active');
                         $(this).parent().toggleClass('is-active');
                     })
-                    .on('blur', function(e) {
+                    .on('blur', function() {
                         setTimeout(() => {
                             $(this).parent().removeClass('is-active');
                         }, 100);
