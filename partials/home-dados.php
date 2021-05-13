3392db9b68aedb333f63577fa18bfd9a21484848
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-13 10:45:17 -0300

    Adatação dos dados da home para o Bulma

--- partials/home-dados.php
+++ partials/home-dados.php
@@ -1,13 +1,13 @@
-<section class="home-dados">
+<section class="section home-dados">
     <div class="container">
         <h2 class="home-dados__titulo"><?php bloginfo('name'); ?> em n&uacute;meros</h2>
-        <div class="home-dados__content">
-            <article class="dado">
+        <div class="columns">
+            <article class="column">
                 <?php $registros = wp_count_posts( 'registro' )->publish ?? 0; ?>
                 <p class="dado__numero"><?php echo $registros; ?></p>
                 <p class="dado__texto"><?php echo _n('Data registrada', 'Datas registradas', $registros, 'ifrs-memoria-theme'); ?> na Linha do Tempo</p>
             </article>
-            <article class="dado">
+            <article class="column">
                 <?php $colecoes = wp_count_posts( 'tainacan-collection' )->publish ?? 0; ?>
                 <p class="dado__numero"><?php echo $colecoes; ?></p>
                 <p class="dado__texto"><?php echo _n('Coleção', 'Coleções', $colecoes, 'ifrs-memoria-theme'); ?> no Acervo</p>
@@ -30,7 +30,7 @@
 
                 wp_reset_postdata();
             ?>
-            <article class="dado">
+            <article class="column">
                 <p class="dado__numero"><?php echo $total; ?></p>
                 <p class="dado__texto"><?php echo _n('Item', 'Itens', $total, 'ifrs-memoria-theme'); ?> inseridos nas Cole&ccedil;&otilde;es</p>
             </article>
