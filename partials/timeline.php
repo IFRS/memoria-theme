b6461593c2022bb4d55f9893195225848bb8bc3d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-13 10:45:58 -0300

    Adaptação da timeline para o Bulma

--- partials/timeline.php
+++ partials/timeline.php
@@ -13,109 +13,109 @@
 ?>
 
 <h2 class="sr-only">Linha do Tempo</h2>
-<ul class="ano-list nav" role="tablist">
+<ul class="ano-list" role="tablist">
     <?php $delay = 0; ?>
     <?php foreach ($posts_by_year as $year => $posts) : ?>
-        <li class="ano-list__item nav-item animate__animated animate__backInDown" style="animation-delay: <?php echo $delay; ?>ms">
-            <a class="ano-list__link nav-link<?php echo (reset($posts_by_year) === $posts) ? ' active' : ''; ?>" href="#ano-<?php echo $year; ?>" role="tab" aria-selected="false" data-toggle="tab">
+        <li class="ano-list__item animate__animated animate__backInDown" style="animation-delay: <?php echo $delay; ?>ms">
+            <a class="ano-list__link <?php echo (reset($posts_by_year) === $posts) ? ' active' : ''; ?>" href="#ano-<?php echo $year; ?>" role="tab" aria-selected="false" data-toggle="tab">
                 <?php echo $year; ?>
             </a>
         </li>
         <?php $delay += 20; ?>
     <?php endforeach; ?>
 </ul>
-<div class="tab-content">
+<div class="timeline-list">
     <?php foreach ($posts_by_year as $year => $posts) : ?>
-        <div class="tab-pane <?php echo (reset($posts_by_year) === $posts) ? ' active' : ''; ?>" id="ano-<?php echo $year; ?>" role="tabpanel">
-            <div class="timeline">
-                <?php
-                    uasort($posts, function ($a, $b) {
-                        $a_meta = get_post_meta( $a->ID, '_registro_data', true );
-                        $b_meta = get_post_meta( $b->ID, '_registro_data', true );
+        <div class="timeline <?php echo (reset($posts_by_year) === $posts) ? ' active' : ''; ?>" id="ano-<?php echo $year; ?>" role="tabpanel">
+            <?php
+                uasort($posts, function ($a, $b) {
+                    $a_meta = get_post_meta( $a->ID, '_registro_data', true );
+                    $b_meta = get_post_meta( $b->ID, '_registro_data', true );
 
-                        $a_date = new DateTime();
-                        $a_date->setDate(($a_meta['year']) ? $a_meta['year'] : 0, ($a_meta['month']) ? $a_meta['month'] : 0, ($a_meta['day']) ? $a_meta['day'] : 0);
+                    $a_date = new DateTime();
+                    $a_date->setDate(($a_meta['year']) ? $a_meta['year'] : 0, ($a_meta['month']) ? $a_meta['month'] : 0, ($a_meta['day']) ? $a_meta['day'] : 0);
 
-                        $b_date = new DateTime();
-                        $b_date->setDate(($b_meta['year']) ? $b_meta['year'] : 0, ($b_meta['month']) ? $b_meta['month'] : 0, ($b_meta['day']) ? $b_meta['day'] : 0);
+                    $b_date = new DateTime();
+                    $b_date->setDate(($b_meta['year']) ? $b_meta['year'] : 0, ($b_meta['month']) ? $b_meta['month'] : 0, ($b_meta['day']) ? $b_meta['day'] : 0);
 
-                        if ($a_date == $b_date) {
-                            return 0;
-                        }
-                        return ($a_date < $b_date) ? -1 : 1;
-                    });
-                ?>
-                <?php foreach ($posts as $post) : ?>
-                    <div class="registro">
-                        <div class="card">
-                            <?php if (has_post_thumbnail($post->ID)) : ?>
-                                <div class="embed-responsive embed-responsive-21by9">
-                                    <?php echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'registro__image img-fluid card-img-top embed-responsive-item animate__animated animate__fadeIn animate__delay-1s')); ?>
-                                </div>
-                            <?php endif; ?>
-                            <div class="card-body">
-                                <?php
-                                    $data = get_post_meta( $post->ID, '_registro_data', true );
-                                    $diames = ($data['month']) ? (($data['day']) ? $data['day'] . ' de ' . date_i18n('F', mktime(0, 0, 0, $data['month'])) : date_i18n('F', mktime(0, 0, 0, $data['month']))) : '';
-                                    $diamesano = ($diames) ? $diames . ' de ' . $data['year'] : $data['year'];
-                                ?>
-                                <?php if (!empty($diames)) : ?>
-                                    <span class="registro__date" data-toggle="tooltip" data-placement="top" title="<?php echo $diamesano; ?>"><?php echo $diames; ?></span>
-                                <?php endif; ?>
-                                <h3 class="registro__title"><a href="<?php echo get_the_permalink($post->ID); ?>" data-toggle="modal" data-target="#modal-<?php echo $post->ID; ?>"><?php echo $post->post_title; ?></a></h3>
-                                <div class="registro__text"><?php echo get_the_excerpt($post->ID); ?></div>
-                                <?php $unidades = get_the_terms($post->ID, 'unidade'); ?>
-                                <?php if (!is_tax('unidade')) : ?>
-                                    <?php if (!empty($unidades)) : ?>
-                                        <?php foreach ($unidades as $unidade) : ?>
-                                            <a class="registro__link" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
-                                        <?php endforeach; ?>
-                                    <?php endif; ?>
-                                <?php endif; ?>
+                    if ($a_date == $b_date) {
+                        return 0;
+                    }
+                    return ($a_date < $b_date) ? -1 : 1;
+                });
+            ?>
+            <?php foreach ($posts as $post) : ?>
+                <div class="registro<?php echo (has_post_thumbnail()) ? '' : ' registro--without-thumbnail' ?>">
+                    <div class="card">
+                        <?php if (has_post_thumbnail($post->ID)) : ?>
+                            <div class="card-image">
+                                <figure class="image is-16by9">
+                                    <?php echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'registro__image animate__animated animate__fadeIn animate__delay-1s')); ?>
+                                </figure>
                             </div>
+                        <?php endif; ?>
+                        <div class="card-content">
+                            <?php
+                                $data = get_post_meta( $post->ID, '_registro_data', true );
+                                $diames = ($data['month']) ? (($data['day']) ? $data['day'] . ' de ' . date_i18n('F', mktime(0, 0, 0, $data['month'])) : date_i18n('F', mktime(0, 0, 0, $data['month']))) : '';
+                                $diamesano = ($diames) ? $diames . ' de ' . $data['year'] : $data['year'];
+                            ?>
+                            <?php if (!empty($diames)) : ?>
+                                <span class="registro__date" data-toggle="tooltip" data-placement="top" title="<?php echo $diamesano; ?>"><?php echo $diames; ?></span>
+                            <?php endif; ?>
+                            <h3 class="registro__title"><a href="<?php echo get_the_permalink($post->ID); ?>" data-toggle="modal" data-target="#modal-<?php echo $post->ID; ?>"><?php echo $post->post_title; ?></a></h3>
+                            <div class="registro__text"><?php echo get_the_excerpt($post->ID); ?></div>
+                            <?php $unidades = get_the_terms($post->ID, 'unidade'); ?>
+                            <?php if (!is_tax('unidade')) : ?>
+                                <?php if (!empty($unidades)) : ?>
+                                    <?php foreach ($unidades as $unidade) : ?>
+                                        <a class="registro__link" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            <?php endif; ?>
                         </div>
                     </div>
-                    <?php add_action('wp_footer', function() use ($post, $unidades, $diamesano) { ?>
-                        <!-- Modal -->
-                        <div class="modal fade" id="modal-<?php echo $post->ID; ?>" tabindex="-1" role="dialog" aria-labelledby="modal-<?php echo $post->ID; ?>-title" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4 class="modal-title" id="modal-<?php echo $post->ID; ?>-title"><?php echo $post->post_title; ?></h4>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
+                </div>
+                <?php add_action('wp_footer', function() use ($post, $unidades, $diamesano) { ?>
+                    <!-- Modal -->
+                    <div class="modal" id="modal-<?php echo $post->ID; ?>" tabindex="-1" role="dialog" aria-labelledby="modal-<?php echo $post->ID; ?>-title" aria-hidden="true">
+                        <div class="modal-background"></div>
+                        <div class="modal-card">
+                            <div class="modal-card-head">
+                                <h4 class="modal-card-title" id="modal-<?php echo $post->ID; ?>-title"><?php echo $post->post_title; ?></h4>
+                            </div>
+                            <div class="modal-card-body">
+                                <div class="columns">
+                                    <div class="column">
+                                        <p>
+                                        <?php foreach ($unidades as $unidade) : ?>
+                                            <a class="registro__link align-self-start" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
+                                        <?php endforeach; ?>
+                                        </p>
                                     </div>
-                                    <div class="modal-body">
-                                        <div class="container-fluid">
-                                            <div class="row">
-                                                <div class="col-12 col-md-6">
-                                                    <p>
-                                                    <?php foreach ($unidades as $unidade) : ?>
-                                                        <a class="registro__link align-self-start" href="<?php echo get_term_link($unidade); ?>"><?php echo $unidade->name; ?></a>
-                                                    <?php endforeach; ?>
-                                                    </p>
-                                                </div>
-                                                <div class="col-12 col-md-6"><p class="text-right"><strong><?php echo $diamesano; ?></strong></p></div>
-                                            </div>
-                                            <?php if (has_post_thumbnail($post->ID)) : ?>
-                                                <div class="registro-detalhe__image">
-                                                    <a href="<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>"><?php echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'img-fluid')); ?></a>
-                                                    <p class="registro-detalhe__caption"><?php echo get_the_post_thumbnail_caption($post->ID); ?></p>
-                                                </div>
-                                            <?php endif; ?>
-                                            <?php echo apply_filters('the_content', $post->post_content); ?>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <small><strong>Atualizado em: </strong><?php echo get_the_modified_date('', $post->ID); ?> &agrave;s <?php echo get_the_modified_time('', $post->ID); ?></small>
+                                    <div class="column">
+                                        <p class="text-right"><strong><?php echo $diamesano; ?></strong></p>
                                     </div>
                                 </div>
+                                <?php if (has_post_thumbnail($post->ID)) : ?>
+
+                                        <figure class="image">
+                                            <a href="<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>">
+                                                <?php echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'img-fluid')); ?>
+                                            </a>
+                                            <figcaption><?php echo get_the_post_thumbnail_caption($post->ID); ?></figcaption>
+                                        </figure>
+                                <?php endif; ?>
+                                <?php echo apply_filters('the_content', $post->post_content); ?>
+                            </div>
+                            <div class="modal-card-foot">
+                                <small><strong>Atualizado em: </strong><?php echo get_the_modified_date('', $post->ID); ?> &agrave;s <?php echo get_the_modified_time('', $post->ID); ?></small>
                             </div>
                         </div>
-                    <?php }); ?>
-                <?php endforeach; ?>
-            </div>
+                        <button class="modal-close is-large" aria-label="close"></button>
+                    </div>
+                <?php }); ?>
+            <?php endforeach; ?>
         </div>
     <?php endforeach; ?>
 </div>
--- src/timeline.js
+++ src/timeline.js
@@ -22,6 +22,7 @@ let observer = new IntersectionObserver((entries) => {
                 entry.target.querySelector('.card').classList.add('animate__fadeInRight');
             }
         } else {
+            /* Para sconder novamente os cards assim que saem do viewport. */
             // entry.target.querySelector('.card').classList.remove('animate__animated', 'animate__fadeInLeft', 'animate__fadeInRight');
         }
     });
@@ -32,3 +33,63 @@ document.addEventListener("DOMContentLoaded", function() {
         .forEach(registro => { observer.observe(registro) }
     );
 });
+
+document.querySelectorAll('.ano-list__link').forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let alvo = document.querySelector(e.target.getAttribute("href"));
+
+        document.querySelectorAll('.timeline').forEach(function(timeline) {
+            timeline.classList.remove('active');
+
+            if (timeline === alvo) {
+                alvo.classList.add('animate__animated', 'animate__fadeIn', 'active');
+            }
+        });
+
+        document.querySelectorAll('.ano-list__link').forEach(function(other) {
+            other.classList.remove('active');
+        });
+
+        e.target.classList.add('active');
+
+        return false;
+    });
+});
+
+document.querySelectorAll('[data-toggle=modal]').forEach(function(modal_toggle) {
+    modal_toggle.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let modal = e.target.dataset.target;
+
+        if (modal) {
+            document.querySelector(modal).classList.add('is-active');
+            document.documentElement.classList.add('is-clipped');
+        }
+
+        return false;
+    });
+});
+
+document.querySelectorAll('button.modal-close').forEach(function(close) {
+    close.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        e.target.parentElement.classList.remove('is-active');
+        document.classList.remove('is-clipped');
+
+        return false;
+    });
+});
+
+// document.querySelectorAll('modal-background').forEach(function(background) {
+//     background.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         e.target.parentElement.classList.remove('is-active');
+//         document.classList.remove('is-clipped');
+
+//         return false;
+//     });
+// });
