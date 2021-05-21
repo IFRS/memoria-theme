53698c3aae02e253ff659377bd09caf07fcce362
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-21 15:16:51 -0300

    Remoção de fundos padrão e ajuste nas configurações

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -16,16 +16,13 @@ add_action( 'after_setup_theme', function() {
     add_theme_support( 'post-thumbnails' );
 
     // Add theme support for HTML5 Semantic Markup
-    add_theme_support(
-        'html5',
-        array(
-            'search-form',
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption'
-        )
-    );
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ) );
 
     // Add theme support for Responsive Embeds
     add_theme_support( 'responsive-embeds' );
@@ -41,29 +38,13 @@ add_action( 'after_setup_theme', function() {
         'flex-width'             => true,
         'flex-height'            => true,
         'uploads'                => true,
-        'random-default'         => false,
-        'header-text'            => true,
-        'default-text-color'     => '#ffffff',
+        'random-default'         => true,
+        'header-text'            => false,
+        'default-text-color'     => '',
         'wp-head-callback'       => '',
         'admin-head-callback'    => '',
         'admin-preview-callback' => '',
         'video'                  => true,
-        'video-active-callback'  => 'is_front_page || is_home',
-    ) );
-
-    // Default Headers
-    register_default_headers( array(
-        'fundo1' => array(
-            'url'           => '%s/img/fundos/fundo1.jpg',
-            'thumbnail_url' => '%s/img/fundos/fundo1.thumb.jpg',
-            'description'   => __( 'Imagem de Cabeçalho Padrão 1', 'ifrs-memoria-theme' ),
-        ),
-    ) );
-    register_default_headers( array(
-        'fundo2' => array(
-            'url'           => '%s/img/fundos/fundo2.jpg',
-            'thumbnail_url' => '%s/img/fundos/fundo2.thumb.jpg',
-            'description'   => __( 'Imagem de Cabeçalho Padrão 2', 'ifrs-memoria-theme' ),
-        ),
+        'video-active-callback'  => 'is_front_page',
     ) );
 } );
