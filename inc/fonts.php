f7d4f764b7a38dbf77c483f048cd0ff89e7aac21
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-05-25 12:22:36 -0300

    Uso correto da função para pegar o caminho do tema

--- inc/fonts.php
+++ inc/fonts.php
@@ -1,8 +1,8 @@
 <?php
 add_action('wp_head', function() {
-    $fonts_file = esc_url(get_template_directory_uri()). '/css/fonts.css';
+    $fonts_file = esc_url(get_stylesheet_directory_uri()). '/css/fonts.css';
 ?>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/signika/signika-v12-latin-300.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/fonts/signika/signika-v12-latin-300.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
     <link rel="preload" href="<?php echo $fonts_file; ?>" as="style"/>
     <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
     <noscript>
