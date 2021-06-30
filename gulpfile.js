90868d678f1633b1f1444dc6e4089cdedf3328a4
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-06-30 15:22:51 -0300

    Migração para dart-sass e ajustes necessários

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const argv         = require('minimist')(process.argv.slice(2));
-const autoprefixer = require('autoprefixer');
 const babel        = require('gulp-babel');
 const browserSync  = require('browser-sync').create();
 const csso         = require('gulp-csso');
@@ -7,10 +6,9 @@ const concat       = require('gulp-concat');
 const del          = require('del');
 const gulp         = require('gulp');
 const path         = require('path');
-const pixrem       = require('pixrem');
 const PluginError  = require('plugin-error');
 const postcss      = require('gulp-postcss');
-const sass         = require('gulp-sass');
+const sass         = require('gulp-sass')(require('sass'));
 const sourcemaps   = require('gulp-sourcemaps');
 const uglify       = require('gulp-uglify');
 const webpack      = require('webpack');
@@ -19,36 +17,27 @@ gulp.task('clean', async function() {
     return await del(['css/', 'js/', 'dist/']);
 });
 
-const postCSSplugins = [
-    pixrem(),
-    autoprefixer()
-];
-
 gulp.task('sass', function() {
-    return gulp.src('sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
+    let postCSS_plugins = [
+        require('pixrem'),
+        require('autoprefixer'),
+    ];
+
+    let sass_options = {
         includePaths: ['sass', 'node_modules'],
         outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(postcss(postCSSplugins))
+    };
+
+    return gulp.src('sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync(sass_options).on('error', sass.logError))
+    .pipe(postcss(postCSS_plugins))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('css/'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('vendor-css', function() {
-    return gulp.src([
-        'node_modules/animate.css/animate.css',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
-    ])
-    .pipe(concat('vendor.css'))
-    .pipe(postcss(postCSSplugins))
-    .pipe(gulp.dest('css/'))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('styles', gulp.series('sass', 'vendor-css', function css() {
+gulp.task('styles', gulp.series('sass', function css() {
     return gulp.src(['css/*.css'])
     .pipe(csso())
     .pipe(gulp.dest('css/'))
@@ -74,7 +63,6 @@ gulp.task('webpack', function(done) {
         resolve: {
             alias: {
                 jquery: 'jquery/src/jquery',
-                bootstrap: 'bootstrap/dist/js/bootstrap.bundle',
             }
         },
         plugins: [
@@ -122,9 +110,7 @@ gulp.task('scripts', gulp.series('webpack', function js() {
             ]
         ]
     }))
-    .pipe(uglify({
-        ie8: true,
-    }))
+    .pipe(uglify())
     .pipe(gulp.dest('js/'))
     .pipe(browserSync.stream());
 }));
@@ -148,7 +134,7 @@ gulp.task('dist', function() {
 if (argv.production) {
     gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'scripts'), 'dist'));
 } else {
-    gulp.task('build', gulp.series('clean', gulp.parallel('sass', 'vendor-css', 'webpack')));
+    gulp.task('build', gulp.series('clean', gulp.parallel('sass', 'webpack')));
 }
 
 const proxyURL = argv.URL || argv.url || 'localhost';
