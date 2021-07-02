752e3f1031adb0d1957c57ffff8f78c0ae5f2880
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-07-02 16:33:19 -0300

    Uso da lib uuid para gerar ids únicas para o lightbox

--- src/modules/lightbox.js
+++ src/modules/lightbox.js
@@ -1,10 +1,12 @@
 require('lightbox2');
+import { v4 as uuidv4 } from 'uuid';
 
 document.addEventListener('DOMContentLoaded', function() {
     let galleries = document.querySelectorAll('.wp-block-gallery');
     galleries.forEach(function(gallery) {
+        let id = uuidv4();
         gallery.querySelectorAll('a').forEach(function(imageLink) {
-            imageLink.dataset.lightbox = 'gallery';
+            imageLink.dataset.lightbox = id;
             imageLink.dataset.title = imageLink.querySelector('img').getAttribute('alt');
         })
     });
@@ -12,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let images = document.querySelectorAll('a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"],a[href$=".gif"],a[href$=".svg"]');
     images.forEach(function(imageLink) {
         if (!imageLink.closest('.wp-block-gallery') && !imageLink.closest('.tainacan-media-component')) {
-            imageLink.dataset.lightbox = 'single';
+            imageLink.dataset.lightbox = uuidv4();
             imageLink.dataset.title = imageLink.querySelector('img').getAttribute('alt');
         }
     });
