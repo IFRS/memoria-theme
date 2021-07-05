abb0298c1f5f7c45985bb192247de5284c58f301
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-07-05 15:34:31 -0300

    Adição de link para página completa de registro da linha do tempo

--- partials/timeline.php
+++ partials/timeline.php
@@ -113,8 +113,11 @@
                                 <?php endif; ?>
                                 <?php echo apply_filters('the_content', $post->post_content); ?>
                             </div>
-                            <div class="modal-card-foot">
+                            <div class="modal-card-foot is-justify-content-space-between">
                                 <small><strong>Atualizado em: </strong><?php echo get_the_modified_date('', $post->ID); ?> &agrave;s <?php echo get_the_modified_time('', $post->ID); ?></small>
+                                <a href="<?php echo get_the_permalink($post->ID); ?>" class="has-tooltip-arrow has-tooltip-left" data-tooltip="Abrir página completa">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-browser" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><rect x="4" y="4" width="16" height="16" rx="1" /><line x1="4" y1="8" x2="20" y2="8" /><line x1="8" y1="4" x2="8" y2="8" /></svg>
+                                </a>
                             </div>
                         </div>
                         <button class="modal-close is-large" aria-label="close"></button>
