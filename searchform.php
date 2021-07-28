8469dc1bb5a237acf7eaa495d891ce541c7b171b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-07-28 11:04:45 -0300

    Ajuste na posição do label para não interferir no input

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <?php $field_id = uniqid('search-'); ?>
 <form role="search" method="get" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
+    <label class="is-sr-only" for="<?php echo $field_id; ?>">Buscar por:</label>
     <div class="field has-addons">
-        <label class="is-sr-only" for="<?php echo $field_id; ?>">Buscar por:</label>
         <div class="control">
             <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="<?php echo $field_id; ?>" class="input is-small searchform__field" placeholder="Busque uma memória..." required>
         </div>
