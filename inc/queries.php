06a2f9fc5adeee9a5d90c3a9c5a030a3c48986b5
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-08-09 13:46:01 -0300

    Ajustes na verificação das queries personalizadas

--- inc/queries.php
+++ inc/queries.php
@@ -1,6 +1,7 @@
 <?php
 add_action('pre_get_posts', function($query) {
-	if ($query->is_main_query() & !is_admin()) {
+	if (!is_admin() && $query->is_main_query()) {
+		/* Timeline */
 		if ($query->is_post_type_archive('registro') || $query->is_tax('unidade')) {
 			$query->query_vars['posts_per_page'] = -1;
 			$query->query_vars['nopaging'] = true;
